1cf97d628c9af220a92ba617e4ebff351f2ec9f3
Ibai Velada López <ibai.velada@opendeusto.es>
2026-03-12 10:23:29 +0100

    first commit

---
+++ src/main/java/com/example/deusto_hotel/DeustoHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.deusto_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeustoHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeustoHotelApplication.class, args);
+    }
+
+}
