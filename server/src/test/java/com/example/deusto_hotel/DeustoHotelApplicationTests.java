fd5e0ce2521f26bc290e6673a46a39a105cf1686
Ibai Velada <ibai.velada@opendeusto.es>
2026-03-19 12:39:55 +0100

    Creacion de proyecto modular terminado

---
+++ server/src/test/java/com/example/deusto_hotel/DeustoHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deusto_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeustoHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
