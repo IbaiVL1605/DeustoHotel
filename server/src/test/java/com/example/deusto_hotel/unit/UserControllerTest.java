62380c89b4ef7e5236c7407c51ca7f443e2cae69
marta.meso@opendeusto.es <marta.meso@opendeusto.es>
2026-04-19 16:47:50 +0200

    Implementación tests UserController

--- server/src/test/java/com/example/deusto_hotel/unit/UserControllerTest.java
+++ server/src/test/java/com/example/deusto_hotel/unit/UserControllerTest.java
@@ -1,9 +1,76 @@
 package com.example.deusto_hotel.unit;
 
+import com.example.deusto_hotel.controller.UserController;
+import com.example.deusto_hotel.dto.UserRequest;
+import com.example.deusto_hotel.dto.UserResponse;
+import com.example.deusto_hotel.model.Role;
+import com.example.deusto_hotel.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserControllerTest {
+class UserControllerTest {
+    @Mock
+    UserService userService;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @InjectMocks
+    UserController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void registrarUsuario_Success() throws Exception {
+
+        UserRequest request = new UserRequest(
+                "Juan",
+                "a@gmail.com",
+                "12345678"
+        );
+
+        UserResponse response = new UserResponse(
+                1L,
+                "Juan",
+                "a@gmail.com",
+                Role.CLIENT,
+                false,
+                LocalDateTime.now()
+        );
+
+        when(userService.create(any(UserRequest.class)))
+                .thenReturn(response);
+
+        mockMvc.perform(post("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.nombre").value("Juan"))
+                .andExpect(jsonPath("$.email").value("a@gmail.com"))
+                .andExpect(jsonPath("$.rol").value("CLIENT"))
+                .andExpect(jsonPath("$.bloqueado").value(false))
+                .andExpect(jsonPath("$.creadoEn").exists());
+    }
 }
--- server/src/test/java/com/example/deusto_hotel/unit/UserServiceTest.java
+++ server/src/test/java/com/example/deusto_hotel/unit/UserServiceTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
