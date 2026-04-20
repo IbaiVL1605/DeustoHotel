15102dae0ff034af9ef4d70628e28dda6cccdbb4
Ibai Velada <ibai.velada@opendeusto.es>
2026-04-20 19:08:46 +0200

    Testeo controller get Disponibles finalizado y comprobado

--- server/src/test/java/com/example/deusto_hotel/unit/HabitacionesControllerTest.java
+++ server/src/test/java/com/example/deusto_hotel/unit/HabitacionesControllerTest.java
@@ -1,8 +1,7 @@
 package com.example.deusto_hotel.unit;
 
 import com.example.deusto_hotel.controller.RoomController;
-import com.example.deusto_hotel.dto.RoomRequest;
-import com.example.deusto_hotel.dto.RoomResponse;
+import com.example.deusto_hotel.dto.*;
 import com.example.deusto_hotel.model.Room;
 import com.example.deusto_hotel.model.RoomType;
 import com.example.deusto_hotel.service.RoomService;
@@ -18,14 +17,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RoomController.class)
@@ -34,7 +36,7 @@ class HabitacionesControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockitoBean
     private RoomService roomService;
 
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -74,38 +76,34 @@ class HabitacionesControllerTest {
 
     @Test
     void getDisponibles_Success() throws Exception {
-        LocalDate fechaEntrada = LocalDate.now().minusDays(1);
-        LocalDate fechaSalida = LocalDate.now().plusDays(1);
+        LocalDate fechaEntrada = LocalDate.now().plusDays(1);
+        LocalDate fechaSalida = LocalDate.now().plusDays(4);
 
-        Room room = Room.builder()
-                .id(1L)
-                .tipo(RoomType.INDIVIDUAL)
-                .build();
-        Room room1 = Room.builder()
-                .id(2L)
-                .tipo(RoomType.INDIVIDUAL)
-                .build();
+        List<RoomDisponibleResponse> rooms = new ArrayList<>();
 
-        Room room3 = Room.builder()
-                .id(3L)
-                .tipo(RoomType.DOBLE)
-                .build();
+        rooms.add(new RoomDisponiblesSimplesResponse(RoomType.INDIVIDUAL, 5));
+        rooms.add(new RoomDisponiblesSimplesResponse(RoomType.DOBLE, 2));
 
-        Room room4 = Room.builder()
-                .id(4L)
-                .tipo(RoomType.SUITE)
-                .build();
 
-         RoomResponse roomResponse = new RoomResponse(room.getId(), room.getNumero(), room.getTipo(), room.getCapacidad(), room.getPrecioPorNoche(), room.getEstado());
+        List<SuitResponse> suits = new ArrayList<>();
+        suits.add(new SuitResponse(4, 200, 1));
+        suits.add(new SuitResponse(2, 150, 2));
 
-        when(roomService.getDisponibles(fechaEntrada, fechaSalida)).thenReturn(List.of(roomResponse));
+        rooms.add(new RoomDisponiblesSuitResponse(RoomType.SUITE, suits));
 
-        when(roomService.getDisponibles(fechaEntrada, fechaSalida)).thenReturn(List.of());
+        when(roomService.getDisponibles(fechaEntrada, fechaSalida)).thenReturn(rooms);
+
 
         mockMvc.perform(get("/api/v1/rooms/disponibles")
                         .param("fechaEntrada", fechaEntrada.toString())
                         .param("fechaSalida", fechaSalida.toString()))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isOk()).
+                andExpect(jsonPath("$[0].tipo").value("INDIVIDUAL"))
+                .andExpect(jsonPath("$[0].numero_disponibles").value(5))
+                .andExpect(jsonPath("$[1].tipo").value("DOBLE"))
+                .andExpect(jsonPath("$[1].numero_disponibles").value(2))
+                .andExpect(jsonPath("$[2].tipo").value("SUITE"))
+                .andExpect(jsonPath("$[2].suites.length()").value(2));
     }
 
 
